78e2f10590749fbc441be8585fbe45c9c57c5d54
cvande04 <Caroline.Vanderlee@tufts.edu>
2019-09-21 00:23:26 -0400

    expanded code skeleton

--- behaviortree.py
+++ behaviortree.py
@@ -2,20 +2,36 @@
 
 # Blackboard: Brandon, Caroline has started for testing
 class blackboard:
-    bat = 100
+    BATTERY_LEVEL = 100
+    SPOT = False
+    GENERAL = False
+    DUSTY_SPOT = False
+    HOME_PATH = 0
 
 # Battery Functions: Caroline
-def battery_level(blackboard):
-    if blackboard.bat < 30:
-        print "Less than 30"
+def battery_check(blackboard):
+    print "Battery level is " + str(blackboard.BATTERY_LEVEL)
+    if blackboard.BATTERY_LEVEL < 30:
+        print "Needs charging"
+        go_home(blackboard)
     else:
-        print "30 or more"
+        print "Charge sufficient to begin cleaning"
+        cleaning_function(blackboard)
     return;
 
 # Go Home: Brandon
 def go_home(blackboard):
+    print "Finding docking station to charge"
+    # Once charged:
+    print "Fully charged and ready to begin cleaning"
+    cleaning_function(blackboard)
     return;
 
 # Cleaning Functions: Caroline
+def cleaning_function(blackboard):
+    print "Beginning cleaning function"
+    return;
 
-# Spot Check
+# Spot Check: Both
+def spot_check(blackboard):
+    return;
