2cd948a83821c8ce787944a3e5891c43314c3860
cvande04 <Caroline.Vanderlee@tufts.edu>
2019-09-21 00:15:47 -0400

    added battery_level function

--- behaviortree.py
+++ behaviortree.py
@@ -1,1 +1,21 @@
 # Caroline Vanderlee and Brandon Bell
+
+# Blackboard: Brandon, Caroline has started for testing
+class blackboard:
+    bat = 100
+
+# Battery Functions: Caroline
+def battery_level(blackboard):
+    if blackboard.bat < 30:
+        print "Less than 30"
+    else:
+        print "30 or more"
+    return;
+
+# Go Home: Brandon
+def go_home(blackboard):
+    return;
+
+# Cleaning Functions: Caroline
+
+# Spot Check
