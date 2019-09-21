1cd3922f65ea2c16d66d9af9124ec00f3e856c18
cvande04 <Caroline.Vanderlee@tufts.edu>
2019-09-21 00:59:46 -0400

    restructured conditionals, added more tests

--- behaviortree.py
+++ behaviortree.py
@@ -16,6 +16,7 @@ class blackboard:
         self.HOME_PATH = h_val
 
 # Battery Functions: Caroline
+# COMPLETE
 def battery_check(blackboard):
     print "Battery level is " + str(blackboard.BATTERY_LEVEL)
     if blackboard.BATTERY_LEVEL < 30:
@@ -23,7 +24,7 @@ def battery_check(blackboard):
         go_home(blackboard)
     else:
         print "Charge sufficient to begin cleaning"
-        cleaning_function(blackboard)
+    cleaning_function(blackboard)
     return;
 
 # Go Home: Brandon
@@ -32,7 +33,6 @@ def go_home(blackboard):
     # Once charged:
     blackboard.BATTERY_LEVEL = 100
     print "Fully charged and ready to begin cleaning"
-    cleaning_function(blackboard)
     return;
 
 # Cleaning Functions: Caroline
@@ -44,7 +44,21 @@ def cleaning_function(blackboard):
         print "A clean has been requested"
         if blackboard.SPOT == True:
             print "A spot check has been requested"
+            # Done spot
+            blackboard.SPOT == False
+            print "Spot check completed"
         print "Beginning general clean"
+        if blackboard.BATTERY_LEVEL < 30:
+            print "Needs charging"
+            go_home(blackboard)
+        if blackboard.DUSTY_SPOT == True:
+            print "Dusty spot detected"
+            # Done intensive
+            blackboard.DUSTY_SPOT == False
+            print "Intensive cleaning completed"
+        # Done general
+        blackboard.GENERAL == False
+        print "General clean completed"
     return;
 
 # Spot Check: Both
@@ -52,38 +66,48 @@ def spot_check(blackboard):
     return;
 
 # Tests
+
 print "TEST ONE: Full battery, no commands"
 batt_one = blackboard(100, False, False, False, 0)
 battery_check(batt_one)
 
-print "TEST TWO: 30% battery, no commands"
+print "\nTEST TWO: 30% battery, no commands"
 batt_two = blackboard(30, False, False, False, 0)
 battery_check(batt_two)
 
-print "TEST THREE: Low battery, no commands"
+print "\nTEST THREE: Low battery, no commands"
 batt_three = blackboard(10, False, False, False, 0)
 battery_check(batt_three)
 
-print "TEST FOUR: 30% battery, spot command"
+print "\nTEST FOUR: 30% battery, spot command"
 batt_four = blackboard(30, True, False, False, 0)
 battery_check(batt_four)
 
-print "TEST FIVE: 30% battery, general command"
+print "\nTEST FIVE: 30% battery, general command"
 batt_five = blackboard(30, False, True, False, 0)
 battery_check(batt_five)
 
-print "TEST SIX: 30% battery, both general and spot commands"
+print "\nTEST SIX: 30% battery, both general and spot commands"
 batt_six = blackboard(30, True, True, False, 0)
 battery_check(batt_six)
 
-print "TEST SEVEN: 10% battery, spot command"
+print "\nTEST SEVEN: 10% battery, spot command"
 batt_seven = blackboard(10, True, False, False, 0)
 battery_check(batt_seven)
 
-print "TEST EIGHT: 10% battery, general command"
+print "\nTEST EIGHT: 10% battery, general command"
 batt_eight = blackboard(10, False, True, False, 0)
 battery_check(batt_eight)
 
-print "TEST NINE: 10% battery, both general and spot commands"
+print "\nTEST NINE: 10% battery, both general and spot commands"
 batt_nine = blackboard(10, True, True, False, 0)
 battery_check(batt_nine)
+
+
+print "\nTEST TEN: 10% battery, general / spot / dusty commands"
+batt_ten = blackboard(10, True, True, True, 0)
+battery_check(batt_ten)
+
+print "\nTEST ELEVEN: 10% battery, general / spot / dusty commands, no recharge for battery before clean"
+batt_eleven = blackboard(10, True, True, True, 0)
+cleaning_function(batt_eleven)
