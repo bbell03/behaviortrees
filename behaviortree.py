72ed3092fd5a21cf965af4e03646f4c5472fa74a
cvande04 <Caroline.Vanderlee@tufts.edu>
2019-09-21 00:42:20 -0400

    expanded conditionals, added tests

--- behaviortree.py
+++ behaviortree.py
@@ -42,6 +42,9 @@ def cleaning_function(blackboard):
         print "No cleaning command requested"
     else:
         print "A clean has been requested"
+        if blackboard.SPOT == True:
+            print "A spot check has been requested"
+        print "Beginning general clean"
     return;
 
 # Spot Check: Both
@@ -50,13 +53,37 @@ def spot_check(blackboard):
 
 # Tests
 print "TEST ONE: Full battery, no commands"
-batt_full = blackboard(100, False, False, False, 0)
-battery_check(batt_full)
+batt_one = blackboard(100, False, False, False, 0)
+battery_check(batt_one)
 
 print "TEST TWO: 30% battery, no commands"
-batt_30 = blackboard(30, False, False, False, 0)
-battery_check(batt_30)
+batt_two = blackboard(30, False, False, False, 0)
+battery_check(batt_two)
 
 print "TEST THREE: Low battery, no commands"
-batt_low = blackboard(10, False, False, False, 0)
-battery_check(batt_low)
+batt_three = blackboard(10, False, False, False, 0)
+battery_check(batt_three)
+
+print "TEST FOUR: 30% battery, spot command"
+batt_four = blackboard(30, True, False, False, 0)
+battery_check(batt_four)
+
+print "TEST FIVE: 30% battery, general command"
+batt_five = blackboard(30, False, True, False, 0)
+battery_check(batt_five)
+
+print "TEST SIX: 30% battery, both general and spot commands"
+batt_six = blackboard(30, True, True, False, 0)
+battery_check(batt_six)
+
+print "TEST SEVEN: 10% battery, spot command"
+batt_seven = blackboard(10, True, False, False, 0)
+battery_check(batt_seven)
+
+print "TEST EIGHT: 10% battery, general command"
+batt_eight = blackboard(10, False, True, False, 0)
+battery_check(batt_eight)
+
+print "TEST NINE: 10% battery, both general and spot commands"
+batt_nine = blackboard(10, True, True, False, 0)
+battery_check(batt_nine)
